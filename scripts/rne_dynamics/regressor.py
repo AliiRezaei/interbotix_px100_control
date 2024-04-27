883f009b13a94b1f3cd922ffc71f40592b0b78ea
AliiRezaei <alii.rezzaei@gmail.com>
2024-04-27 15:29:13 -0700

    fix .any() error

--- scripts/rne_dynamics/regressor.py
+++ scripts/rne_dynamics/regressor.py
@@ -20,16 +20,16 @@ def regressor(rbtdef, geom, ifunc=None):
 
     for p, parm in enumerate(dynparms):
 
-        for i in range(rbtdef.dof):
-            rbtdeftmp.Le[i] = list(map(
-                lambda x: 1 if x == parm else 0, rbtdef.Le[i]))
-            rbtdeftmp.l[i] = Matrix(rbtdef.l[i]).applyfunc(
-                lambda x: 1 if x == parm else 0)
-            rbtdeftmp.m[i] = 1 if rbtdef.m[i] == parm else 0
-            rbtdeftmp.Ia[i] = 1 if rbtdef.Ia[i] == parm else 0
-            rbtdeftmp.fv[i] = 1 if rbtdef.fv[i] == parm else 0
-            rbtdeftmp.fc[i] = 1 if rbtdef.fc[i] == parm else 0
-            rbtdeftmp.fo[i] = 1 if rbtdef.fo[i] == parm else 0
+        # for i in range(rbtdef.dof):
+        #     rbtdeftmp.Le[i] = list(map(
+        #         lambda x: 1 if x == parm else 0, rbtdef.Le[i]))
+        #     rbtdeftmp.l[i] = Matrix(rbtdef.l[i]).applyfunc(
+        #         lambda x: 1 if x == parm else 0)
+        #     rbtdeftmp.m[i] = 1 if rbtdef.m[i] == parm else 0
+        #     rbtdeftmp.Ia[i] = 1 if rbtdef.Ia[i] == parm else 0
+        #     rbtdeftmp.fv[i] = 1 if rbtdef.fv[i] == parm else 0
+        #     rbtdeftmp.fc[i] = 1 if rbtdef.fc[i] == parm else 0
+        #     rbtdeftmp.fo[i] = 1 if rbtdef.fo[i] == parm else 0
 
         Y[:, p] = rne_backward(rbtdeftmp, geom, fw_results, ifunc=ifunc)
 
